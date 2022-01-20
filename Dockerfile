4d9e34c16e803cf46592612d905d4845a228f526
ksr-at-289154328210 <seetharamkoya9@gmail.com>
2022-01-21 04:50:45 +0530

    first commit

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:14
-
-WORKDIR /nodejs/app
-
-COPY package*.json ./
-RUN npm install
-
-COPY . .
-CMD [ "node", "index.js" ]
--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express()
-const appId = process.env.APPID;
-
-app.get('/', (req, res) => res.send(`Opening app : ${appId}`));
-
-app.listen(appId, () => console.log(`App listening on port ${appId}!`))
